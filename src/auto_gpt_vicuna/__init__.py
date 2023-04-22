a67be0fa95bdcb3be257840c5ab998f2bbd26a12
becin <cbecin@gmail.com>
2023-04-22 16:27:58 +0000

    f string new line fix

--- src/auto_gpt_vicuna/__init__.py
+++ src/auto_gpt_vicuna/__init__.py
@@ -75,7 +75,7 @@ class AutoGPTPVicuna(AutoGPTPluginTemplate):
             "All output must be in format that can be read by Python json.loads, always"
             " ensure output is in the proper format so it can be read."
     	 )
-
+        new_line = '\n'
         jsonFormat = f"""Always use the following format for your responses:
 
 {{{
@@ -89,7 +89,7 @@ class AutoGPTPVicuna(AutoGPTPluginTemplate):
     {{{
         'text': 'thought',
         'reasoning': 'reasoning',
-        'plan': '- short bulleted\n- list that conveys\n- long-term plan',
+        'plan': '- short bulleted{new_line}- list that conveys{new_line}- long-term plan',
         'criticism': 'constructive self-criticism',
         'speak': 'thoughts summary to say to user'
     }}}
